73b5e462fae6cc8b5a8928581314330ec8d7be87
JD <jd_gonzales@icloud.com>
2024-02-15 21:05:28 -0600

    Initial Commit

---
+++ lib/polidef.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "polidef/version"
+
+module Polidef
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_polidef.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestPolidef < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Polidef::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
